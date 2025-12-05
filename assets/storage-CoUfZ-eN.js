f17e61d15be1e374e110edcc3fd7b13abe88dda0
z <136988523+Freakoverse@users.noreply.github.com>
2025-12-05 20:27:22 +0200

    Add files via upload

---
+++ assets/storage-CoUfZ-eN.js
@@ -0,0 +1,1 @@
+import{d as e,s,g as r}from"./index-faHou9s9.js";const n={getItem:async t=>await r(t)||null,setItem:async(t,a)=>{await s(t,a)},removeItem:async t=>{await e(t)}};export{n as storage};
