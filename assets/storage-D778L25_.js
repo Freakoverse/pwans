b7f0c5168de709374bf7b47fdf1b79da7d4d5799
z <136988523+Freakoverse@users.noreply.github.com>
2025-12-06 17:41:10 +0200

    Add files via upload

---
+++ assets/storage-D778L25_.js
@@ -0,0 +1,1 @@
+import{d as e,s,g as r}from"./index-0YHluwLP.js";const n={getItem:async t=>await r(t)||null,setItem:async(t,a)=>{await s(t,a)},removeItem:async t=>{await e(t)}};export{n as storage};
