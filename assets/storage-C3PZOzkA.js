cd7756c6d5bd59df786c190e8957ddb319ddf53f
z <136988523+Freakoverse@users.noreply.github.com>
2025-12-06 17:15:33 +0200

    Add files via upload

---
+++ assets/storage-C3PZOzkA.js
@@ -0,0 +1,1 @@
+import{d as e,s,a as r}from"./index-lKvOgqv5.js";const n={getItem:async t=>await r(t)||null,setItem:async(t,a)=>{await s(t,a)},removeItem:async t=>{await e(t)}};export{n as storage};
