6db14a61b2753603766126056dbd8311ecf6a8cf
z <136988523+Freakoverse@users.noreply.github.com>
2025-12-07 13:49:35 +0200

    Add files via upload

---
+++ assets/storage-SUx_yxTY.js
@@ -0,0 +1,1 @@
+import{d as e,s,g as r}from"./index-Bqid9IJ8.js";const n={getItem:async t=>await r(t)||null,setItem:async(t,a)=>{await s(t,a)},removeItem:async t=>{await e(t)}};export{n as storage};
