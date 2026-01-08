5b23e36cc1d971ba96b8ed552197e26150ae8ff6
z <136988523+Freakoverse@users.noreply.github.com>
2026-01-08 03:08:20 +0200

    Add files via upload

---
+++ assets/storage-D7x1dBXQ.js
@@ -0,0 +1,1 @@
+import{S as t}from"./index-BQxAXTBT.js";const r={getItem:async e=>t.getItem(e),setItem:async(e,a)=>{await t.setItem(e,a)},removeItem:async e=>{await t.removeItem(e)}};export{r as storage};
